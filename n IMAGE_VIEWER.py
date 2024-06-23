19c6c780cea93702563c0b0ecddb5921a7abcd02
navdeepsingh160 <118738353+navdeepsingh160@users.noreply.github.com>
2024-06-23 10:29:23 +0530

    Add files via upload

---
+++ n IMAGE_VIEWER.py
@@ -0,0 +1,16 @@
+import tkinter
+import ImageTK
+import PIL
+
+
+configure(background="black")
+
+Label(background = "black")
+
+place(Top) 
+
+img_path("/Users/macbookair/Desktop/project147/image.jpeg")
+
+rotateImage(180) 
+
+ImageTk.photoimage()
